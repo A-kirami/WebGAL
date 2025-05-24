c1b6ed95816d49ad62c6a39d0b5444d0d088e589
Akirami <66513481+A-kirami@users.noreply.github.com>
2025-05-24 16:10:16 +0800

    🔖 BandoriCraft v1.1.0

--- packages/webgal/src/UI/Title/Title.tsx
+++ packages/webgal/src/UI/Title/Title.tsx
@@ -166,14 +166,16 @@ const Title: FC = () => {
               }}
               onMouseEnter={playSeEnter}
             >
-              <div className={applyStyle('Title_button_text', styles.Title_button_text)} data-content={t('exit.title')}>{t('exit.title')}</div>
+              <div className={applyStyle('Title_button_text', styles.Title_button_text)} data-content={t('exit.title')}>
+                {t('exit.title')}
+              </div>
             </div>
           </div>
         </div>
       )}
       {GUIState.showTitle && (
         <div className={styles.Title_version_info}>
-          <div>WebGAL BandoriCraft Engine v1.0.0</div>
+          <div>WebGAL BandoriCraft Engine v1.1.0</div>
           <div>(Based on WebGAL v{__INFO.version})</div>
         </div>
       )}
